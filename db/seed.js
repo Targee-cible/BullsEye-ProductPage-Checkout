49dddbd5ce0c1ea1f56ee2b731a8f863c4ef180c
SeanKier <seantkier@gmail.com>
2019-09-13 21:21:58 -0700

    adds seed script to seed myql to 10 million products

--- db/seed.js
+++ db/seed.js
@@ -1,133 +1,88 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+
 const faker = require('faker');
-// const db = require('./connection.js');
-const db = require('./mysql.js')
-// const Locations = require('../models/locations.js');
-// const Inventory = require('../models/inventory.js');
-// const Products = require('../models/products.js');
+const db = require('./mysql.js');
 
 //  Create Size Array
 const sizes = ['S', 'M', 'L', 'XL', '2XL'];
 
 //  Create Color Array with object {name, url}
 //  URL will have default value until i am able to upload my own swatches
-const colors = [
-  {
-    color: 'Red',
-  },
-  {
-    color: 'Blue',
-  },
-  {
-    color: 'Green',
-  },
-  {
-    color: 'Orange',
-  },
-  {
-    color: 'Black',
-  },
-  {
-    color: 'Purple',
-  },
-];
-
-
-const seedData = function () {
-  const sql = "INSERT INTO product (name, price, size, color, numOfRatings, totalNumStars) VALUES ?";
-  const values = [
-    ['TShirts', 9999, 'SML', 'RBGO', 2, 8],
-    ['Pants',     55, 'SML', 'RBGO', 3, 6],
-    ['Socks',   1633, 'SML', 'RBG',  5, 5]
-  ];
-  
-  db.query(sql, [values], function (err, result) {
-    if (err) throw err;
-    console.log("Number of records inserted: " + result.affectedRows);
-  });
-
-  const sqlStore = "INSERT INTO stores (streetAddress, city, zipCode) VALUES ?";
-  const storeData = [
-    ['123 main st', 'city1', 95043],
-    ['23456 fjdfj', 'sdd', 48909]
-  ]
-  db.query(sqlStore, [storeData], function (err, result) {
-    if (err) throw err;
-    console.log("Number of records inserted: " + result.affectedRows);
-  });
-
-  const sqlInventory = "INSERT INTO inventory (store_Id, product_Id, quantity, size, color) VALUES ?";
-  const storeInventory = [
-    [1, 1, 55, 'L', 'B'],
-    [2, 2, 34, 'S', 'O']
-  ]
-  db.query(sqlInventory, [storeInventory], function (err, result) {
-    if (err) throw err;
-    console.log("Number of records inserted: " + result.affectedRows);
-  });
+const colors = ['Red', 'Blue', 'Green', 'Orange', 'Black', 'Purple'];
+const numReviews = function() {
+  return Math.round(Math.random() * 40);
 }
 
+const randomSize = function () {
+  const index = Math.round(Math.random() * 4);
+  return sizes[index];
+}
+const randomColor = function () {
+  const index = Math.round(Math.random() * 5);
+  return colors[index];
+}
+const seedData = function () {
+  const targetSize = 10000000;
+  let currentSize = 0;
+
+  const seedDataHelper = function () {
+    const sql = "INSERT INTO product (name, price, size, color, numOfRatings, totalNumStars) VALUES ?";
+    let values = [
+    ];
+    values = [];
+    for (let i = 0; i < 10000; i++) {
+      const reviews = numReviews();
+      const totalStars = (Math.random() * 5) * reviews;
+      values.push([
+        faker.commerce.productName(),
+        faker.finance.amount(0.01, 50.00, 2),
+        randomSize(),
+        randomColor(),
+        reviews,
+        totalStars
+      ]);
+    }
+    db.query(sql, [values], function (err, result) {
+      if (err) throw err;
+      console.log(`Number of records inserted: ${result.affectedRows} ${currentSize}`);
+      currentSize += result.affectedRows;
+      if (currentSize < targetSize) {
+        values = [];
+        console.log(`Calling again: ${currentSize}`);
+        seedDataHelper();
+      } else {
+        console.log("Done!!!!");
+      }
+    });
+  };
+
+  seedDataHelper();
+};
 
 seedData();
-// /*
-//   Clear Existing Collections
-// */
-// const removeExistingItems = () => new Promise((resolve, reject) => {
-//   const removedItems = [];
-//   removedItems.push(Products.deleteMany({}));
-//   removedItems.push(Locations.deleteMany({}));
-//   removedItems.push(Inventory.deleteMany({}));
-
-//   Promise.all(removedItems)
-//     .then(() => {
-//       resolve();
-//     })
-//     .catch((err) => {
-//       reject(err);
-//     });
-
-
+// const sqlStore = "INSERT INTO stores (streetAddress, city, zipCode) VALUES ?";
+// const storeData = [
+//   ['123 main st', 'city1', 95043],
+//   ['23456 fjdfj', 'sdd', 48909]
+// ]
+// db.query(sqlStore, [storeData], function (err, result) {
+//   if (err) throw err;
+//   console.log("Number of records inserted: " + result.affectedRows);
 // });
 
-// /*
-//   Seed Products Collection
-// */
-// const seedProductCollection = () => new Promise((resolve, reject) => {
-//   const createdItems = [];
-//   //  Loop to Create 100 product Ids
-//   for (let i = 1; i <= 100; i += 1) {
-//     //  Create possible product colors
-//     const availableColors = colors.slice();
-//     const productColors = [];
-//     let randomColors = faker.random.number({ min: 1, max: colors.length });
-
-//     while (randomColors > 0) {
-//       const randIndex = faker.random.number({ min: 0, max: randomColors - 1 });
-//       productColors.push(availableColors[randIndex]);
-//       availableColors.splice(randIndex, 1);
-//       randomColors -= 1;
-//     }
-
-//     //  Construct product object
-//     const product = {
-//       productId: i,
-//       name: faker.commerce.productName(),
-//       price: faker.finance.amount(0.01, 50.00, 2),
-//       size: sizes,
-//       color: productColors,
-//       reviews: [],
-//     };
-
-//     //  Create product
-//     createdItems.push(Products.create(product));
-//   }
-
-//   Promise.all(createdItems)
-//     .then((items) => {
-//       resolve(items);
-//     })
-//     .catch((err) => {
-//       reject(err);
-//     });
+// const sqlInventory = "INSERT INTO inventory (store_Id, product_Id, quantity, size, color) VALUES ?";
+// const storeInventory = [
+//   [1, 1, 55, 'L', 'B'],
+//   [2, 2, 34, 'S', 'O']
+// ]
+// db.query(sqlInventory, [storeInventory], function (err, result) {
+//   if (err) throw err;
+//   console.log("Number of records inserted: " + result.affectedRows);
 // });
 
 // /*
