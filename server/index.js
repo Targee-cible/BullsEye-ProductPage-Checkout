6d0ed80ff9474da18619b12ba60f3a49ba864335
SeanKier <seantkier@gmail.com>
2019-09-23 14:49:01 -0700

    adds lru caching for get products

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
+/* eslint-disable no-const-assign */
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-param-reassign */
 require('newrelic');
 
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const LRU = require('lru-cache');
+const options = { max: 100
+  , length: function (n, key) { return n * 2 + key.length }
+  , dispose: function (key, n) { n.close() }
+  , maxAge: 1000 * 60 * 60
+};
+const cache = new LRU(options);
+const otherCache = new LRU(50);
+
 const db = require('../db/index.js');
 
 
@@ -29,17 +40,25 @@ app.listen(port, () => console.log(`Server started on Port ${port}`));
 
 app.get('/api/checkout/product/:productId', (req, res) => {
   const { productId } = req.params;
-  db.getProduct(productId)
-    .then((product) => {
-      const parseColors = JSON.parse(product.colors);
-      product.colors = parseColors;
-      const parseSizes = JSON.parse(product.size);
-      product.size = parseSizes;
-      res.status(200).json(product);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+  const cacheProduct = cache.get(productId);
+  if (cacheProduct !== undefined) {
+    console.log('>>>>>>>>>>working');
+    res.status(200).send(cacheProduct);
+  } else {
+    db.getProduct(productId)
+      .then((product) => {
+        const parseColors = JSON.parse(product.colors);
+        product.colors = parseColors;
+        const parseSizes = JSON.parse(product.size);
+        product.size = parseSizes;
+        cache.set(productId, product);
+        res.status(200).json(product);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  }
+
 });
 
 app.get('/api/checkout/quantity/:productId&:color&:size&:storeId', (req, res) => {
