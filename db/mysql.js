1f6b8ba0ed185478dcb62b030ab4add09aac50f7
SeanKier <seantkier@gmail.com>
2019-09-24 16:01:35 -0700

    connected to mysql on ec2

--- db/mysql.js
+++ db/mysql.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'target'
+  host: '34.219.176.169',
+  port: 3306,
+  user: 'Sean',
+  password: 'password',
+  database: 'testing',
 });
 
 connection.connect((err) => {
